6fe93b13ef78481f1221d9ad539bdc9b534303bd
Paul Liverman III <paul.liverman.iii@gmail.com>
2017-12-09 08:54:54 -0800

    added Class fn

--- behave.lua
+++ behave.lua
@@ -665,6 +665,43 @@ do
   end
   RunOnce = _class_0
 end
+local Class
+Class = function(name, parent)
+  local newClass, base
+  base = {
+    __index = base,
+    __class = newClass
+  }
+  newClass = setmetable({
+    __init = function() end,
+    __base = base,
+    __name = name
+  }, {
+    __call = function(cls, ...)
+      local self = setmetable({ }, base)
+      cls.__init(self, ...)
+      return self
+    end
+  })
+  if parent then
+    setmetable(base, {
+      __parent = parent.__base
+    })
+    newClass.__parent = parent
+    newClass.__index = function(cls, name)
+      local val = rawget(base, name)
+      if val == nil then
+        return parent[name]
+      else
+        return val
+      end
+    end
+    if parent.__inherited then
+      parent:__inherited(newClass)
+    end
+  end
+  return newClass, base
+end
 local behave = {
   Node = Node,
   Sequence = Sequence,
@@ -679,11 +716,17 @@ local behave = {
   RunOnce = RunOnce,
   success = success,
   running = running,
-  fail = fail
+  fail = fail,
+  Class = Class
 }
 behave.clone = function(object)
+  local new
   local cls = getmetatable(object).__class.__name
-  local new = behave[cls](object)
+  if cls == "Repeat" then
+    new = behave[cls](object.cycles, object)
+  else
+    new = behave[cls](object)
+  end
   if object.nodes then
     local nodes = { }
     for k, v in pairs(object.nodes) do
